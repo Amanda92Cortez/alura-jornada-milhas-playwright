56e68778d54f407b1e17e7d4c486f9060272a149
Amanda Cortez <amanda_cortez_2012@hotmail.com>
2025-02-05 16:28:39 -0300

    Aula 03
    Aula 03

---
+++ e2e/fazerLogin.spec.ts
@@ -0,0 +1,20 @@
+import test from "@playwright/test";
+import PaginaLogin from "./page-objects/PaginaLogin";
+
+test.describe("Página de Login", () => {
+  test("Deve conseguir fazer login com email e senha válidos", async ({ page }) => {
+    const paginaLogin = new PaginaLogin(page);
+
+    await paginaLogin.visitar();
+    await paginaLogin.fazerLogin('antonio.evaldo@alura.com', '123456');
+    await paginaLogin.loginFeitoComSucesso();
+  });
+
+  test("Não deve conseguir fazer login com email inválido", async ({ page }) => {
+    const paginaLogin = new PaginaLogin(page);
+
+    await paginaLogin.visitar();
+    await paginaLogin.fazerLogin('antonio.errado@alura.com', '123456');
+    await paginaLogin.estaMostrandoMensagemDeErro('Você não está autorizado a acessar este recurso');
+  });
+});
