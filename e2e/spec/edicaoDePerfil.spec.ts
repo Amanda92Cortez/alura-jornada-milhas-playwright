5db0301b30892b7e2e1e6fba025485c4e1208e11
Amanda Cortez <amanda_cortez_2012@hotmail.com>
2025-02-06 17:56:31 -0300

    Alura
    Alura

---
+++ e2e/spec/edicaoDePerfil.spec.ts
@@ -0,0 +1,11 @@
+import test from "@playwright/test";
+
+test.describe("Página de perfil", () => {
+    test("Editar perfil 1", () => {
+        // Fazer cadastro
+
+        // Fazer Login
+
+        // Editar perfil
+    })
+})
